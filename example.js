fecb054f71df26f480cd7b844a03cd0e31e728e6
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-14 20:02:18 +0000

    Update example.

--- example.js
+++ example.js
@@ -1,65 +1,77 @@
 
-var dualapi = require('./index');
 
-var domain = dualapi();
+var dualproto = require('./index');
+var domain = dualproto();
 
-domain.mount(['journal', ':name'], function (ctxt) {
-    console.log(ctxt.from.join('/') + ' published to the journal of ' + ctxt.params.name + ' : ', ctxt.body);
-});
-
-domain.send(['journal', 'bioinformatics'], ['scientist'], 'The Human Genome');
-
-domain.mount(['laboratory', 'supercomputer'], function (ctxt) {
-    ctxt.reply('Super Computed: ' + ctxt.body);
-});
-
-domain.mount(['scientist'], function (ctxt) {
-    if (ctxt.from.join('/') === 'laboratory/supercomputer') {
-        domain.send(['journal', 'computation'], ['scientist'], ctxt.body);
+var db = {};
+domain.mount(['database', ':collection'], function (body, ctxt) {
+    var collection = ctxt.params.collection;
+    if (!db.hasOwnProperty(collection)) {
+      db[collection] = [];
     }
-    else {
-        domain.send(['laboratory', 'supercomputer'], ['scientist'], ctxt.body);
-    }
+    db[collection].push(ctxt.body);
 });
 
-domain.send(['scientist'], [], 'sequence alignment');
+domain.send(['database', 'message'], [], 'Hello Alice!');
 
-domain.get(['laboratory', 'supercomputer'], 'test data')
+domain.mount(['message', ':name'], function (body, ctxt) {
+    console.log(ctxt.from.join('/') + ' sent a message to ' + ctxt.params.name);
+    console.log('The message was received by ' + ctxt.from.join('/'));
+    console.log('The message is: ', ctxt.body);
+    ctxt.domain.send(['database', 'message'], [], ctxt.body);
+});
+
+domain.send(['message', 'alice'], ['user', 'bob'], 'Hello Alice!');
+
+/*
+user/bob sent a message to alice
+The message was received by message/alice
+The message is: Hello Alice!
+*/
+
+domain.waitFor(['ready'], { timeout: 60 })
 .then(function (ctxt) {
-    console.log('super computer result: ', ctxt.body);
+  console.log('We are ready!');
 });
 
-var io = require('./examples/mock-io');  //  pretends to be socket.io
+domain.send(['ready']);
+/*
+We are ready!
+*/
 
-// on the server side
-io.listen().on('connect', function (socket) {
-    domain.open(['alice'], socket);
-    domain.send(['alice', 'ready']);
+
+domain.uid().then(function (mailbox) {
+  domain.waitFor([mailbox])
+  .then(function (ctxt) {
+    console.log('My temporary mailbox just received: ', ctxt.body);
+  });
+
+  domain.send([mailbox], [], 'mail!');
 });
 
-// on the client side
+/*
+My temporary mailbox just received:  mail!
+*/
 
-var aliceDomain = dualapi();
-var socket = io.connect();
-aliceDomain.open(['server'], socket);
-
-aliceDomain.mount(['ready'], function () {
-    aliceDomain.send(['server', 'scientist'], ['tip'], 'Alice data');
+var api = dualproto.use(function (dualproto) {
+  dualproto.Message.prototype.reply = function (body) {
+     this.domain.send(this.from, [], body);
+  };
+});
+var domain = api();
+domain.mount(['responder'], function (body, ctxt) {
+  ctxt.reply('Hello!');
 });
 
-aliceDomain.mount(['supercomputer'], function (ctxt) {
-    ctxt.forward(['server', 'laboratory', 'supercomputer']);
+domain.uid().then(function (mailbox) {
+  domain.waitFor([mailbox])
+  .then(function (ctxt) {
+    console.log('Responder said: ', ctxt.body);
+  });
+
+  domain.send(['responder'], [mailbox], 'hello?');
 });
 
-aliceDomain.mount(['ready'], function () {
-    aliceDomain.get(['supercomputer'], 'alice super computer data')
-    .then(function (ctxt) {
-        console.log('client side super computer: ', ctxt.body);
-    });
-});
-
-
-
-
-
-
+/*
+Responder said:  Hello!
+*/
